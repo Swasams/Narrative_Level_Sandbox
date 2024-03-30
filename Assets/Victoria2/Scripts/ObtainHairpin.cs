c4ebc9c694e2df5c77fe63281dc6455b654cbea5
Swasams <92123037+Swasams@users.noreply.github.com>
2024-03-30 13:39:42 -0400

    Revert "Merge branch 'main' into Victoria"
    This reverts commit 905a7ab0fea8de1fc32d8a6d8513657c54c4f97d, reversing
    changes made to 8a6fe2ab73b3d204bd5ac0a925530adc076911cf.

---
+++ Assets/Victoria2/Scripts/ObtainHairpin.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//public class ClickableObject : MonoBehaviour
+{
+    public GameObject hairpinPrefab; 
+    public Transform canvasTransform; 
+    public GameObject targetUIObject; 
+
+    public void OnClickDresser()
+    {
+        if (targetUIObject != null)
+        {
+          
+            GameObject instantiatedHairpin = Instantiate(hairpinPrefab, canvasTransform);
+
+          
+            Vector3 relativePosition = targetUIObject.transform.position - canvasTransform.position;
+            instantiatedHairpin.transform.localPosition = relativePosition;
+
+            
+        }
+    }
+}
